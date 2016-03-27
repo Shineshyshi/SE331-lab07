9c468491841a639c3ca2d69c8bbe83f96e6b54a0
Auttapon <auttapon_n@cmu.ac.th>
2016-03-27 21:48:20 +0700

    version0.6

--- app/js/shoppingCartController.js
+++ app/js/shoppingCartController.js
@@ -1,15 +1,12 @@
-/**
- * Created by Comproz on 3/27/2016 AD.
- */
 'use strict';
 
 var shoppingCartControllers = angular.module('shoppingCartControllers', ['shoppingCartServices']);
 
 shoppingCartControllers.controller('showShoppingCartController',
-    ['$scope', 'shoppingCartService', '$location', '$rootScope','$routeParams',
-        function ($scope, shoppingCartService, $location, $rootScope,$rootParams) {
+    ['$scope', 'shoppingCartService', '$location', '$rootScope', '$routeParams',
+        function ($scope, shoppingCartService, $location, $rootScope, $rootParams) {
             var id = $rootParams.id;
-            shoppingCartService.get({id:id},function(data){
+            shoppingCartService.get({id: id}, function (data) {
                 $scope.cart = data;
             });
 
@@ -19,23 +16,23 @@ shoppingCartControllers.controller('showShoppingCartController',
             });
 
             $scope.updateCart = function () {
-                shoppingCartService.update({id:id},$scope.cart,function(){
+                shoppingCartService.update({id: id}, $scope.cart, function () {
                     $rootScope.cartUpdateSuccess = true;
 
                 });
             };
 
-            $scope.totalEach = function(index){
+            $scope.totalEach = function (index) {
+
                 return $scope.cart.selectedProducts[index].product.totalPrice * $scope.cart.selectedProducts[index].amount;
             };
 
-            $scope.total = function(){
+            $scope.total = function () {
                 var total = 0;
-                angular.forEach($scope.cart.selectedProducts, function(item) {
+                angular.forEach($scope.cart.selectedProducts, function (item) {
                     total += item.amount * item.product.totalPrice;
                 });
 
                 return total;
             }
         }]);
-
